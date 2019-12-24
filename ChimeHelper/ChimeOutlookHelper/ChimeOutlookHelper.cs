cd0c524d886e55cc5b1a78d911096974c3cecc72
nachmore <code@nachmore.com>
2019-12-24 12:48:12 +1100

    Basic iteration through calendar to find Chime messages
    Doesn't do much beyond that at this point. Upcoming:
    
    1. Expose this to the UI
    2. Tests

---
+++ ChimeHelper/ChimeOutlookHelper/ChimeOutlookHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Interop.Outlook;
+using System.Text.RegularExpressions;
+
+namespace ChimeOutlookHelper
+{
+  public class ChimeOutlookHelper
+  {
+
+    public struct ChimeMeeting
+    {
+      public string Subject { get; set; }
+      public DateTime StartTime { get; set; }
+      public DateTime EndTime { get; set; }
+      public HashSet<string> Meetings { get; set; }
+    }
+
+    public static List<ChimeMeeting> GetMeetings()
+    {
+      var calendar = OutlookHelper.GetCalendar();
+
+      var appointments = OutlookHelper.GetAppointmentsAroundNow(calendar, 5);
+
+      var meetings = new List<ChimeMeeting>();
+
+      foreach (Outlook.AppointmentItem appointment in appointments)
+      {
+        var bridges = GetBridges(appointment);
+
+        if (bridges.Count > 0)
+        {
+          meetings.Add(
+            new ChimeMeeting()
+            {
+              Subject = appointment.Subject,
+              StartTime = appointment.Start,
+              EndTime = appointment.End,
+              Meetings = bridges
+            }
+          );
+        }
+      }
+
+      return meetings;
+    }
+
+    private static HashSet<string> GetBridges(AppointmentItem appointment)
+    {
+      var rv = new List<string>();
+
+      rv.AddRange(GetBridgeFromLocation(appointment.Location));
+      rv.AddRange(GetBridgeFromBody(appointment.Body));
+
+      return new HashSet<string>(rv);
+    }
+
+    private static List<string> GetBridgeFromLocation(string location)
+    {
+      var rv = new List<string>();
+
+      // find chime bridge as whole number
+
+      var re = new Regex(@"\d[\d ]{9,}", RegexOptions.Multiline);
+      var matches = re.Matches(location);
+
+      foreach (Match match in matches)
+      {
+        rv.Add(match.Value.Replace(" ", ""));
+      }
+
+      return rv;
+    }
+
+    // There are too many random numbers in invitation bodies (especially embedded links)
+    // to be able to use the simple regex from GetBridgeFromLocation
+    private static List<string> GetBridgeFromBody(string body)
+    {
+      var rv = new List<string>();
+
+      if (string.IsNullOrEmpty(body))
+        return rv;
+
+      // Chime is currently only English, but I have to assume that that will change in the future
+
+      // try to find an ID in a URL
+      var re = new Regex(@"chime.aws/(\d{9,})", RegexOptions.Multiline);
+      var matches = re.Matches(body);
+
+      foreach (Match match in matches)
+      {
+        rv.Add(match.Groups[1].Value);
+      }
+
+      // if we couldn't find any URLs try more coarse matching
+      if (rv.Count == 0)
+      {
+        rv = GetBridgeFromLocation(body);
+      }
+
+      return rv;
+    }
+  }
+}
+
---
+++ ChimeHelper/ChimeOutlookHelper/OutlookHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Diagnostics;
+
+namespace ChimeOutlookHelper
+{
+  static class OutlookHelper
+  {
+
+    public static Outlook.MAPIFolder GetCalendar()
+    {
+      var outlook = new Outlook.Application();
+      var stores = outlook.Session.Stores;
+
+      foreach (Outlook.Store store in stores)
+      {
+        try
+        {
+
+          Outlook.MAPIFolder folder = store.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar);
+          System.Diagnostics.Debug.WriteLine(folder.Name);
+
+          return folder;
+        }
+        catch (Exception e)
+        {
+          // Not every root folder has a calendar (for example, Public folders), so this exceptionc can be ignored
+          Debug.WriteLine("Failed to get Calendar:\n" + e);
+        }
+      }
+
+      throw new InvalidOperationException("Couldn't find a Calendar in the current Outlook installation");
+    }
+
+    public static Outlook.Items GetAppointmentsAroundNow(Outlook.MAPIFolder calendar, int hours = 1)
+    {
+      var start = DateTime.Now.Subtract(new TimeSpan(7,hours, 0, 0));
+      var end = start.Add(new TimeSpan(hours * 2, 0, 0));
+
+      return GetAppointmentsInRange(calendar, start, end);
+    }
+
+    public static Outlook.Items GetAppointmentsInRange(Outlook.MAPIFolder folder, DateTime start, DateTime end, bool includeRecurrences = true)
+    {
+      var filter = $"[Start] >= '{start.ToString("g")}' AND [End] <= '{end.ToString("g")}'";
+
+      var items = folder.Items;
+      items.IncludeRecurrences = includeRecurrences;
+      items.Sort("[Start]", Type.Missing);
+
+      return items.Restrict(filter);
+    }
+
+  }
+}
