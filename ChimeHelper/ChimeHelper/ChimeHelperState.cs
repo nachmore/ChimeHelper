837d8e8d57628d3312adb76fe842e5f22541a032
nachmore <code@nachmore.com>
2020-01-05 23:25:25 +1100

    Background Timer to refresh meeting status
    Currently hardcoded, will look into configuration in the future.

--- ChimeHelper/ChimeHelper/ChimeHelperState.cs
+++ ChimeHelper/ChimeHelper/ChimeHelperState.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static ChimeHelper.ChimeHelperTray;
 
 namespace ChimeHelper
 {
@@ -12,12 +14,6 @@ namespace ChimeHelper
   class ChimeHelperState
   {
     private static ChimeHelperState _instance;
-
-    private ChimeHelperState() 
-    {
-        
-    }
-
     public static ChimeHelperState Create()
     {
       if (_instance == null)
@@ -26,7 +22,33 @@ namespace ChimeHelper
       return _instance;
     }
 
+    private Timer _timer;
 
-
+    private ChimeHelperState() 
+    {
+      StartMeetingTimer();
     }
+
+    public void StartMeetingTimer()
+    {
+      _timer = new Timer(CheckForChimeMeetings, null, 1000, 15 * 60 * 1000);
+    }
+
+    public void CheckForChimeMeetings(object stateInfo)
+    {
+      var meetings = ChimeOutlookHelper.ChimeOutlookHelper.GetMeetings();
+      var _meetings = new ChimeMeetingMenuItems<ChimeMeetingMenuItem>();
+
+      foreach (var meeting in meetings)
+      {
+        _meetings.AddRange(ChimeMeetingMenuItem.Create(meeting));
+      }
+
+      App.Current.Dispatcher.Invoke(new Action(() =>
+      {
+        App.TrayIcon.DataContext = (_meetings.Count > 0 ? _meetings : ChimeHelperTray.NO_MEETINGS);
+      }));
+    }
+
   }
+}
