c776743931f6ec267b4e8c2caffbd12e462aa960
Oren Nachman <code@nachmore.com>
2020-01-06 02:11:00 +1100

    About Window with text and version binding

--- ChimeHelper/ChimeHelper/AboutWindow.xaml.cs
+++ ChimeHelper/ChimeHelper/AboutWindow.xaml.cs
@@ -14,18 +14,33 @@ namespace ChimeHelper
   public partial class AboutWindow : Window
   {
     public static AboutWindow _aboutWindow;
-    private AboutWindow()
-    {
-      InitializeComponent();
-    }
 
     public static void CreateAndShow()
     {
-      if (_aboutWindow == null || _aboutWindow.IsLoaded == false) 
+      if (_aboutWindow == null || _aboutWindow.IsLoaded == false)
         _aboutWindow = new AboutWindow();
 
       _aboutWindow.Show();
       _aboutWindow.Activate();
+    }
+
+    public string Version { get; set; }
+
+    private AboutWindow()
+    {
+      InitializeComponent();
+
+      InitVersion();
+
+      DataContext = this;
+    }
+
+    private void InitVersion()
+    {
+      var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+      var buildDate = new DateTime(2000, 1, 1).AddDays(version.Build).AddSeconds(version.Revision * 2).ToString("u");
+
+      Version = $"{version} ({buildDate})";
     }
   }
 }
