ec5eab3f3af75400347c16bf6b6f5d9a943035be
nachmore <code@nachmore.com>
2020-01-06 14:41:43 +1100

    UX improvements to hyper links and handling the no-meetings state
    * BoolToObjectConverter for easy visibility conversions
    * HyperlinkAuto - an extension to the Hyperlink control that opens links
                      without needing a specific codebehind handler

---
+++ ChimeHelper/ChimeHelper/XAMLExtenders/BoolToObjectConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ChimeHelper.XAMLExtenders
+{
+  /// <summary>
+  /// Signature based on https://docs.microsoft.com/en-us/dotnet/api/microsoft.toolkit.uwp.ui.converters.booltoobjectconverter?view=win-comm-toolkit-dotnet-stable&viewFallbackFrom=uwp-toolkit-dotnet
+  /// (no implementation code was looked at / copied)
+  /// 
+  /// Basic converter that takes a True and a False value and returns the correct one based on the input value
+  /// </summary>
+  class BoolToObjectConverter : IValueConverter
+  {
+
+    public object TrueValue { get; set; }
+    public object FalseValue { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var boolVal = (bool)value;
+
+      if (boolVal)
+        return TrueValue;
+      else
+        return FalseValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ChimeHelper/ChimeHelper/XAMLExtenders/HyperlinkAuto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace ChimeHelper.XAMLExtenders
+{
+  class HyperlinkAuto : Hyperlink
+  {
+    public HyperlinkAuto() : base()
+    {
+      this.RequestNavigate += HyperlinkAuto_RequestNavigate;
+    }
+
+    private void HyperlinkAuto_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+      if (NavigateUri != null)
+      {
+        Process.Start(new ProcessStartInfo(NavigateUri.AbsoluteUri));
+      }
+    }
+
+  }
+}
