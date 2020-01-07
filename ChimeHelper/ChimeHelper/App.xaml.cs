c3bf7fb92a09c06043ff37c6f93fe2de17a3efde
nachmore <code@nachmore.com>
2020-01-07 20:49:37 +1100

    Use a synch Event to shutdown ChimeHelper during InstallScript
    This replaces (the now removed) KillProcWMI plugin which wasn't working
    and replaces it with direct Win32 calls to create a synchronization
    event which the installer signals via SetEvent() triggering a
    shutdown.

--- ChimeHelper/ChimeHelper/App.xaml.cs
+++ ChimeHelper/ChimeHelper/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
 using System.Windows;
 
 namespace ChimeHelper
@@ -17,12 +21,42 @@ namespace ChimeHelper
 
       ShutdownMode = ShutdownMode.OnExplicitShutdown;
 
+      InitExitEventWaiter();
+
       TrayIcon = (TaskbarIcon)FindResource("ChimeHelperTaskbarIcon");
 
       new ChimeHelperTray(TrayIcon);
       ChimeHelperState.Create();
 
       SettingsWindow.LoadSettings();
+    }
+
+    /// <summary>
+    /// Monitors a global mutex that can be used to signal this process to exit which
+    /// can be useful for the installer to shut down our process
+    /// </summary>
+    private void InitExitEventWaiter()
+    {
+      // create a rule that allows anybody in the "Users" group to synchronise with us
+      var users = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+      var rule = new EventWaitHandleAccessRule(users, EventWaitHandleRights.Synchronize | EventWaitHandleRights.Modify,
+                                AccessControlType.Allow);
+      var security = new EventWaitHandleSecurity();
+      security.AddAccessRule(rule);
+      bool createdNew;
+
+      var ewh = new EventWaitHandle(false, EventResetMode.ManualReset, @"Global\nachmore.ChimeHelper.IsRunning", out createdNew, security);
+
+      new Thread(() =>
+      {
+        ewh.WaitOne();
+
+        Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+        {
+          Application.Current.Shutdown();
+        }));
+      }).Start();
+
     }
 
   }
