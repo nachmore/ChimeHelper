e3652f60e48a69bab458870da0631021b90e438c
Oren Nachman <code@nachmore.com>
2020-01-24 12:49:20 +1100

    Refactored pin detection logic into ChimeHelper
    Also removed access to GetFreeformPinsFromText() since it was already called as a backup from GetPinsForText()

--- ChimeHelper/ChimeHelper/Chime.cs
+++ ChimeHelper/ChimeHelper/Chime.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ChimeHelper
 {
@@ -11,5 +13,68 @@ namespace ChimeHelper
       System.Diagnostics.Process.Start(String.Format(MEETING_URL_FORMAT, pin));
     }
 
+    /// <summary>
+    /// Retrieves any numbers from text that look like they may be a Chime pin.
+    ///
+    /// Ignores: numbers in URLs that don't also contain the word "chime"
+    /// Ignores: long phone numbers that start with + (for country code), but will catch regular
+    ///          phone numbers
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    private static List<string> GetFreeformPinsFromText(string text)
+    {
+      var rv = new List<string>();
+
+      if (string.IsNullOrEmpty(text))
+        return rv;
+
+      // find chime pin as whole number
+
+      var re = new Regex(@"(?<!https://(?!chime)+\S+)(?<![\+\d\s]+)\s?\d[\d ]{9,}", RegexOptions.Multiline);
+      var matches = re.Matches(text);
+
+      foreach (Match match in matches)
+      {
+        rv.Add(match.Value.Replace(" ", ""));
+      }
+
+      return rv;
+    }
+
+    /// <summary>
+    /// Retrieve Pins from text
+    ///
+    /// This will initially try to grab pins from chime URLs (the most accurate), but will fall back
+    /// to more fuzzy matching if none are found.
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static List<string> GetPinsFromText(string text)
+    {
+      var rv = new List<string>();
+
+      if (string.IsNullOrEmpty(text))
+        return rv;
+
+      // Chime is currently only English, but I have to assume that that will change in the future
+
+      // try to find an ID in a URL
+      var re = new Regex(@"chime.aws/(\d{9,})", RegexOptions.Multiline);
+      var matches = re.Matches(text);
+
+      foreach (Match match in matches)
+      {
+        rv.Add(match.Groups[1].Value);
+      }
+
+      // if we couldn't find any URLs try more coarse matching
+      if (rv.Count == 0)
+      {
+        rv = GetFreeformPinsFromText(text);
+      }
+
+      return rv;
+    }
   }
 }
--- ChimeHelper/ChimeOutlookHelper/ChimeOutlookHelper.cs
+++ ChimeHelper/ChimeOutlookHelper/ChimeOutlookHelper.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Microsoft.Office.Interop.Outlook;
-using System.Text.RegularExpressions;
 using System.Diagnostics;
+using ChimeHelper;
 
 namespace ChimeOutlookHelper
 {
@@ -52,62 +52,14 @@ namespace ChimeOutlookHelper
 
       Debug.WriteLine(appointment.Start + " -> " + appointment.End + ": " + appointment.Subject + "\n\tLocation: " + appointment.Location);
 
-      rv.AddRange(GetPinsFromText(appointment.Location));
-      rv.AddRange(GetPinsFromBody(appointment.Body));
+      rv.AddRange(Chime.GetPinsFromText(appointment.Location));
+      rv.AddRange(Chime.GetPinsFromText(appointment.Body));
 
       Debug.WriteLine("\tPins: " + string.Join("\n\t", rv));
 
       return new HashSet<string>(rv);
     }
 
-    private static List<string> GetPinsFromText(string location)
-    {
-      var rv = new List<string>();
-
-      if (string.IsNullOrEmpty(location))
-        return rv;
-
-      // find chime pin as whole number
-
-      var re = new Regex(@"(?<!https://(?!chime)+\S+)(?<![\+\d\s]+)\s?\d[\d ]{9,}", RegexOptions.Multiline);
-      var matches = re.Matches(location);
-
-      foreach (Match match in matches)
-      {
-        rv.Add(match.Value.Replace(" ", ""));
-      }
-
-      return rv;
-    }
-
-    // There are too many random numbers in invitation bodies (especially embedded links)
-    // to be able to use the simple regex from GetPinsFromLocation
-    private static List<string> GetPinsFromBody(string body)
-    {
-      var rv = new List<string>();
-
-      if (string.IsNullOrEmpty(body))
-        return rv;
-
-      // Chime is currently only English, but I have to assume that that will change in the future
-
-      // try to find an ID in a URL
-      var re = new Regex(@"chime.aws/(\d{9,})", RegexOptions.Multiline);
-      var matches = re.Matches(body);
-
-      foreach (Match match in matches)
-      {
-        rv.Add(match.Groups[1].Value);
-      }
-
-      // if we couldn't find any URLs try more coarse matching
-      if (rv.Count == 0)
-      {
-        rv = GetPinsFromText(body);
-      }
-
-      return rv;
-    }
   }
 }
 
