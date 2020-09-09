948a7934b3191bf4bb0e3de0cf9101a89c0963b4
Oren Nachman <code@nachmore.com>
2020-09-09 14:00:57 +1000

    FindAncestor should find logical parents, not internal objects

--- ChimeHelper/ChimeHelperUX/UXHelpers/AncestorHelper.cs
+++ ChimeHelper/ChimeHelperUX/UXHelpers/AncestorHelper.cs
@@ -1,45 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
 
 namespace ChimeHelperUX.UXHelpers
 {
   static class AncestorHelper
   {
-    // Modified from: https://faithlife.codes/blog/2008/02/finding_ancestor_elements_in_w/
-
-    public static DependencyObject GetParent(DependencyObject obj)
-    {
-      if (obj == null)
-        return null;
-
-      if (obj is ContentElement ce)
-      {
-        DependencyObject parent = ContentOperations.GetParent(ce);
-        
-        if (parent != null)
-          return parent;
-
-
-        return ce is FrameworkContentElement fce ? fce.Parent : null;
-      }
-      return VisualTreeHelper.GetParent(obj);
-    }
-
     public static T FindAncestor<T>(DependencyObject obj)
       where T : DependencyObject
     {
-      // always find a parent (or null)
-      obj = GetParent(obj);
+      var popupRootFinder = VisualTreeHelper.GetParent((DependencyObject)obj);
 
-      while (obj != null)
+      while (popupRootFinder != null)
       {
-        if (obj is T objTest)
-          return objTest;
+        var logicalRoot = LogicalTreeHelper.GetParent(popupRootFinder);
 
-        obj = GetParent(obj);
+        if (logicalRoot is T t)
+        {
+          return t;
+        }
+
+        popupRootFinder = VisualTreeHelper.GetParent(popupRootFinder);
       }
 
       return null;
