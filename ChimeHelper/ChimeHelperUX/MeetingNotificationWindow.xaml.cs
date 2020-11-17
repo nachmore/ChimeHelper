fcd28a0f4ed8cc1ecdc049021372e6883c0a21c7
nachmore <code@nachmore.com>
2020-11-18 07:57:41 +1100

    Initial work towards a "meeting starting" notification window

---
+++ ChimeHelper/ChimeHelperUX/MeetingNotificationWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ChimeHelperUX.UXHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChimeHelperUX
+{
+  /// <summary>
+  /// Interaction logic for Settings.xaml
+  /// </summary>
+  public partial class MeetingNotificationWindow : Window
+  {
+
+    #region Singleton
+    public static MeetingNotificationWindow _meetingNotificationWindow;
+
+    // Not using "Instance" since this Singleton is more of an action
+    public static void CreateAndShow()
+    {
+      if (_meetingNotificationWindow == null || _meetingNotificationWindow.IsLoaded == false)
+        _meetingNotificationWindow = new MeetingNotificationWindow();
+
+      _meetingNotificationWindow.Show();
+      _meetingNotificationWindow.Activate();
+      _meetingNotificationWindow.Topmost = true;
+
+      WindowPositioner.MoveToMouse(_meetingNotificationWindow);
+    }
+    #endregion
+
+    private MeetingNotificationWindow()
+    {
+      InitializeComponent();
+
+      DataContext = this;
+    }
+  }
+}
--- ChimeHelper/ChimeHelperUX/Properties/Settings.Designer.cs
+++ ChimeHelper/ChimeHelperUX/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ChimeHelperUX.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace ChimeHelperUX.Properties {
             }
             set {
                 this["UpgradeRequired"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool NotifyOnMeetingStart {
+            get {
+                return ((bool)(this["NotifyOnMeetingStart"]));
+            }
+            set {
+                this["NotifyOnMeetingStart"] = value;
             }
         }
     }
