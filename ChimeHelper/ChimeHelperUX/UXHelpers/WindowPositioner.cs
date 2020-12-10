f44d5fb4d82f69bf06268afa7fabfdc63674ea64
nachmore <code@nachmore.com>
2020-12-11 10:19:14 +1100

    Better window positioning based on mouse
    P/Invoke GetCursorPos() to get the real cursor position, allowing this
    to work even when the original window does not currently have mouse
    focus.

--- ChimeHelper/ChimeHelperUX/UXHelpers/WindowPositioner.cs
+++ ChimeHelper/ChimeHelperUX/UXHelpers/WindowPositioner.cs
@@ -1,21 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace ChimeHelperUX.UXHelpers
 {
   static class WindowPositioner
   {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool GetCursorPos(ref Win32Point pt);
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Win32Point
+    {
+      public Int32 X;
+      public Int32 Y;
+    };
+    public static Point GetMousePosition()
+    {
+      var w32Mouse = new Win32Point();
+      GetCursorPos(ref w32Mouse);
+
+      return new Point(w32Mouse.X, w32Mouse.Y);
+    }
+
     public static void MoveToMouse(Window target, int leftOffset = 0, int topOffset = 0, int offscreenBuffer = 20)
     {
-      var transform = PresentationSource.FromVisual(target).CompositionTarget.TransformFromDevice;
-      var mouse = transform.Transform(GetMousePosition(target));
+      var mouse = GetMousePosition();
 
       target.Left = mouse.X - target.ActualWidth + leftOffset;
       target.Top = mouse.Y - target.ActualHeight + topOffset;
@@ -32,19 +45,5 @@ namespace ChimeHelperUX.UXHelpers
       if (target.Top + target.Height > SystemParameters.VirtualScreenTop + SystemParameters.VirtualScreenHeight)
         target.Top = SystemParameters.VirtualScreenTop + SystemParameters.VirtualScreenHeight - target.Height - offscreenBuffer;
     }
-
-    /// <summary>
-    /// Retrieve the current mouse position as a System.Windows.Point (instead of the default System.Drawing.Point)
-    /// </summary>
-    /// <returns></returns>
-    private static Point GetMousePosition(Control target)
-    {
-      var point = Mouse.GetPosition(target);
-      point = target.PointToScreen(point);
-
-      return new Point(point.X, point.Y);
-    }
-
-
   }
 }
